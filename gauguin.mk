2bfbe1505602d14ca5f237277bad0744e2ceddd3
Demon Singur <demonsingur@gmail.com>
2019-04-17 16:29:09 +0800

    gauguin: Import fingerprint HIDL
    Change-Id: I733eea70dc637fa61c2e3a8036100de7febd71a9
    Signed-off-by: TH779 <i@779.moe>

--- gauguin.mk
+++ gauguin.mk
@@ -129,6 +129,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_gauguin
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
