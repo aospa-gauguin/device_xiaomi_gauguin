b8818eb41072eea8d0609e1f6498744c755b429a
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    gauguin: releasetools: Ship and update vbmeta image
    Change-Id: I8bb4da99945eceae3beab77489e6b9e2a47ab972

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,6 +124,9 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
