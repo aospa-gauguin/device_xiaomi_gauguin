4e997fa2cf68ae24363bb540d44422cb8a8c3136
Neeraj Soni <neersoni@codeaurora.org>
2019-06-06 20:36:41 +0530

    gauguin: Enable file based encryption
    Provide flag to override double encryption for external storage data.
    
    Change-Id: I96f296e122f2e536ba1d7f6aa6ab9e297aa47c9c

--- device.mk
+++ device.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Keymaster
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.volume.filenames_mode = "aes-256-cts"
+
 # MIDI feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
