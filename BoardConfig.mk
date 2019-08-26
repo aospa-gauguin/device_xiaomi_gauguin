d66784bab624855c4e2a75f78705d566d4953aad
AbhiShek Aggarwal <warabhishek@gmail.com>
2019-08-26 17:40:14 -0400

    gauguin: Add custom init to set proper variants
    Change-Id: I04f09a14cf41c89b3135c53e3089d5c3e703aae0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,10 @@ BOARD_KERNEL_PAGESIZE := 4096
 
 KERNEL_DEFCONFIG := gauguin_defconfig
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_gauguin
+TARGET_RECOVERY_DEVICE_MODULES := libinit_gauguin
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
