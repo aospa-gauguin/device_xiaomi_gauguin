fc23a10f5c1803e9546607b3a2236ca4d5996da0
TH779 <i@779.moe>
2021-08-24 13:04:10 +0800

    gauguin: Import aidl QTI vibrator HAL from LA.UM.9.12.r1-11300-SMxx50.0
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I55828e7e445c4dc3d5382298dfec3c86d804bbd1

--- gauguin.mk
+++ gauguin.mk
@@ -260,6 +260,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.xiaomi_gauguin
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
