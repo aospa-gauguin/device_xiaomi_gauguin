ef9ab738cb07900beaad47e5fabb7a42a8885d0c
Dyneteve <dyneteve@hentaios.com>
2022-01-24 12:00:19 +0530

    gauguin: Build vendor HIDL interfaces
    * In a perfect world google expects us to build all the hals from source however this isn't the case here.
    The HIDL interfaces got removed by default from VNDK and moved to vendor so as a result we need to build them ourselves
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I2da8edb6e5912ab618df78a0797cfaf51fc9f50a

--- device.mk
+++ device.mk
@@ -225,6 +225,13 @@ PRODUCT_SOONG_NAMESPACES += \
 # Vendor
 $(call inherit-product, vendor/xiaomi/gauguin/gauguin-vendor.mk)
 
+# VNDK
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.4.vendor \
+    android.hardware.gatekeeper@1.0.vendor \
+    android.hardware.keymaster@4.1.vendor \
+    android.hardware.neuralnetworks@1.3.vendor
+
 # WLAN
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
