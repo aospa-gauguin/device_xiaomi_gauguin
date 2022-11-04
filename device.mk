d3719bbe281fc220d4b96200d77557a93a9f303a
Omkar Chandorkar <gotenksIN@aospa.co>
2022-11-04 18:03:48 +0900

    gauguin: move common bt packages to common repo
    Change-Id: Iac75cfff2b3edeb741032ee3811f15a7e6c2b200
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- device.mk
+++ device.mk
@@ -39,17 +39,6 @@ PRODUCT_ODM_PROPERTIES += \
     vendor.audio.offload.track.enable=false
 
 # Bluetooth
-PRODUCT_PACKAGES += \
-    android.hardware.bluetooth.audio@2.0-impl \
-    audio.bluetooth.default \
-    com.dsi.ant@1.0.vendor \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
-    libbluetooth_audio_session \
-    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
-    vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
-    vendor.qti.hardware.fm@1.0.vendor
-
 PRODUCT_VENDOR_PROPERTIES += \
     persist.sys.fflag.override.settings_bluetooth_hearing_aid=true \
     persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac-aptxadaptiver2 \
