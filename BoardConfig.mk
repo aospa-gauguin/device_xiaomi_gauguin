c77d9fee33dbf66c95410817bab7ef2a9eba24da
hridaya <hridaya@pixelos.net>
2023-02-13 17:25:33 +0545

    gauguin: Add bootloader board name
    Change-Id: I2acf86d8a7248ca8d097fedfc60cf00d322781c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := lito
+
 # Display
 TARGET_SCREEN_DENSITY := 440
 
