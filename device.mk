e20e71f5aed1204a4dbb5988c3037a5950b55d9a
hridaya <hridaya@pixelos.net>
2023-02-14 17:21:11 +0545

    gauguin: Import WCNSS config from LA.UM.9.12.r1-14400-SMxx50.QSSI12.0
    Change-Id: I12962f609b6749415d57205e61ae06f9bcd861b5

--- device.mk
+++ device.mk
@@ -92,3 +92,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/gauguin/gauguin-vendor.mk)
+
+# WLAN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
