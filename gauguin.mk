bb8eff4800d9b2ff81f806cf5322b6b3a04c3b31
ReallySnow <reallysnow233@gmail.com>
2023-04-20 20:15:29 +0800

    gauguin: Import WCNSS configuration from LA.UM.9.12.r1-15300-SMxx50.QSSI12.0
    Change-Id: Ife78419f770fb9c1ea73d7138842ed637f77f57f

--- gauguin.mk
+++ gauguin.mk
@@ -111,3 +111,7 @@ PRODUCT_PACKAGES += \
 # Verified Boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
+# WLAN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wlan/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
