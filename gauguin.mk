b96a7d1702389b00f23281844e181e8b9586cba1
ReallySnow <reallysnow233@gmail.com>
2023-04-20 19:56:00 +0800

    gauguin: Build IR HIDL
    Change-Id: I778a46086e440c49e7088370d6100eb4a64514cf

--- gauguin.mk
+++ gauguin.mk
@@ -57,6 +57,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
+# IR
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
 # Namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
